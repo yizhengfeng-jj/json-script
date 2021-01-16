3c41536cec5d52e02cd57fcabdb0f558b27400fa
lvjie04 <lvjie04@baidu.com>
2021-01-16 17:32:10 +0800

    feature:增加了字体功能

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
-console.log('aaa');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import './App.less';
+
+class App extends React.Component {
+    render() {
+        return <div className="container">
+            <div><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/154c9711cfee46b68400f4f1eedb9983~tplv-k3u1fbpfcp-watermark.image" /></div>
+            <div className="title">这是风恋猫的脚手架，持续更新中敬请期待...</div>
+            <div className="title">updating...</div>
+        </div>
+    }
+}
+
+ReactDom.render(<App />, document.getElementById('root'))
