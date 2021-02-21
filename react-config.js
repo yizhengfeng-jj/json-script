9b41468c7b777f1c0c7d0cd011f62637115441b8
lvjie04 <lvjie04@baidu.com>
2021-02-21 16:46:27 +0800

    feat:增加--port和proxy配置文件

---
+++ react-config.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//     serverConfig: {
+//         proxySuffix: '/api1',
+//         proxyTarget: 'http://localhost:5555',
+//         proxy: {
+//             '/test': {
+//                 target: "http://localhost:7778",
+//                 changeOrigin: true,
+//                 pathRewrite: {
+//                     '^/test': ''
+//                 }
+//             }
+//         }
+//     }
+// }
--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import axios from 'axios';
 
 import './App.less';
 
 class App extends React.Component {
+    componentDidMount() {
+        axios.get('/api1/get/list').then(res => {
+            console.log(res, 'res.......res....');
+        });
+
+        axios.get('/test/get/demo').then(res => {
+            console.log(res, 'res.......res....');
+        });
+    }
     render() {
         return <div className="container">
             <div><img src="https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/154c9711cfee46b68400f4f1eedb9983~tplv-k3u1fbpfcp-watermark.image" /></div>
